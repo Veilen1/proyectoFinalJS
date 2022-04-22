f51ff04a3aa41d90d295668349d926db01876d8f
Veilen1 <devrientv@gmail.com>
2022-04-21 21:48:56 -0300

    First commit: objects and arrays added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,45 @@
-const ethereum = 3017;
-const bitcoin = 39988;
-const dogecoin = 0.14;
-const polygon = 1.37;
-const riotpoints = 0.00704;
+
+let coins = [
+    {
+    nombre : "ethereum",
+    precio : 3017
+},
+{
+    nombre : "bitcoin",
+    precio : 39988
+},
+{
+    nombre : "dogecoin",
+    precio : 0.14
+},
+{
+    nombre : "polygon",
+    precio : 1.37
+},
+{
+    nombre : "riotpoints",
+    precio : 0.00704
+},
+];
+
 alert("Bienvenido a la calculadora de criptomonedas, los precios son unitarios, estan en dolares y te los muestro a continuación:\nEthereum: 3017$\nBitcoin: 39988$\nDogeCoin: 0.14$\nPoligon: 1.37$\nRiotpoints: 0.00704$")
 
 function calculo(moneda, cantidad){
     switch(moneda){
         case "ethereum":
-            return ethereum * cantidad;
+            return coins[0].precio * cantidad;
             break;
         case "bitcoin":
-                return bitcoin * cantidad;
+            return coins[1].precio * cantidad;
                 break;
         case "dogecoin":
-                return dogecoin * cantidad;
+            return (coins[2].precio * cantidad).toFixed(2);
                 break;
         case "polygon":
-                return polygon * cantidad;
+            return (coins[3].precio * cantidad).toFixed(2);
                 break;
         case "riotpoints":
-                return riotpoints * cantidad;
+            return (coins[4].precio * cantidad).toFixed(3);
                 break;
         default:
             return false;
