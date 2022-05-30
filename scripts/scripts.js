3877aebac97043dd85b1008080b83b185523b716
Veilen1 <devrientv@gmail.com>
2022-05-30 18:30:41 -0300

    Inicializando Api Coingecko y desestructurando

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 // INICIALIZAR API COINGECKO CRYPTOS Y IDS
 const contenedor = document.getElementById("contenedor")
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false", ).then((response) =>{
+fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false", ).then((response) =>{
     return  response.json(); 
 }).then( (cryptos) => {
     cryptos.forEach(cryptos => {
